3386afa5dd8a3ab8cf49fb1401d62af30ea36d05
Sawan Sunar <Sawansunar24072002@gmail.com>
2023-12-14 05:27:16 +0530

    Added my own plugin

---
+++ lua/plugins/alter.lua
@@ -0,0 +1,14 @@
+return {
+    dir = "/home/sawan/projects/luaPlugin/alter/",
+    event = "VeryLazy",
+    config = function()
+        local alter = require('alter')
+        alter:setup()
+        local map = vim.keymap.set
+        map("n", "<leader>af", function() alter:AddFile() end, { desc = "Alter add file" })
+        map("n", "<leader>ac", function() alter:ConnectFile() end, { desc = "Alter connect file" })
+        map("n", "<leader>ap", function() alter:PrintConnection() end, { desc = "Alter alternate file" })
+        map("n", "<leader>aP", function() alter:PrintAll() end, { desc = "Alter all connected project files" })
+        map("n", "<C-b>", function() alter:Alternate() end, { desc = "Alter alternate file" })
+    end
+}
