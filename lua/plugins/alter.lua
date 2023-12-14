53c4986bc5875067e8096b16ca404f8c42788503
Sawan Sunar <Sawansunar24072002@gmail.com>
2023-12-15 00:05:56 +0530

    Alter keymap: Delete connection

--- lua/plugins/alter.lua
+++ lua/plugins/alter.lua
@@ -7,8 +7,9 @@ return {
         local map = vim.keymap.set
         map("n", "<leader>af", function() alter:AddFile() end, { desc = "Alter add file" })
         map("n", "<leader>ac", function() alter:ConnectFile() end, { desc = "Alter connect file" })
-        map("n", "<leader>ap", function() alter:PrintConnection() end, { desc = "Alter alternate file" })
+        map("n", "<leader>ap", function() alter:PrintConnection() end, { desc = "Alter print alternate file" })
         map("n", "<leader>aP", function() alter:PrintAll() end, { desc = "Alter all connected project files" })
+        map("n", "<leader>ad", function() alter:DeleteConnetion() end, { desc = "Alter delete connection of current file" })
         map("n", "<C-b>", function() alter:Alternate() end, { desc = "Alter alternate file" })
     end
 }
