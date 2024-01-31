0eb5f9d03488d395e30fe71f545b11277616daa2
Sawan Sunar <Sawansunar24072002@gmail.com>
2024-02-01 03:03:02 +0530

    Added treesj and fixed mini splitjoin

---
+++ lua/plugins/treesj.lua
@@ -0,0 +1,10 @@
+return {
+    'Wansmer/treesj',
+    dependencies = 'nvim-treesitter',
+    keys = {
+        { '<leader>cj', '<cmd>TSJToggle<cr>', desc = 'Join/split code block' },
+    },
+    opts = {
+        use_default_keymaps = false,
+    },
+}
