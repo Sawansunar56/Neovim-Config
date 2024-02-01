82f0555cbac26f63a36a738256be15b62d55eafe
Sawan Sunar <Sawansunar24072002@gmail.com>
2024-02-01 16:12:07 +0530

    Fixing

--- lua/plugins/treesj.lua
+++ lua/plugins/treesj.lua
@@ -3,6 +3,8 @@ return {
     dependencies = 'nvim-treesitter',
     keys = {
         { '<leader>cj', '<cmd>TSJToggle<cr>', desc = 'Join/split code block' },
+        { '<leader>cp', '<cmd>TSJSplit<cr>',  desc = 'split code block' },
+        { '<leader>ct', '<cmd>TSJJoin<cr>',   desc = 'Join code block' },
     },
     opts = {
         use_default_keymaps = false,
