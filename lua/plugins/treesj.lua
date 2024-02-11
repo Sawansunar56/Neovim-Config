aece5a0bfac38f41966408f1c1af425e5a141307
Sawan Sunar <Sawansunar24072002@gmail.com>
2024-02-12 00:10:59 +0530

    format

--- lua/plugins/treesj.lua
+++ lua/plugins/treesj.lua
@@ -1,10 +1,10 @@
 return {
-    'Wansmer/treesj',
-    dependencies = 'nvim-treesitter',
+    "Wansmer/treesj",
+    dependencies = "nvim-treesitter",
     keys = {
-        { '<leader>cj', '<cmd>TSJToggle<cr>', desc = 'Join/split code block' },
-        { '<leader>cp', '<cmd>TSJSplit<cr>',  desc = 'split code block' },
-        { '<leader>ct', '<cmd>TSJJoin<cr>',   desc = 'Join code block' },
+        { "<leader>cj", "<cmd>TSJToggle<cr>", desc = "Join/split code block" },
+        { "<leader>cp", "<cmd>TSJSplit<cr>",  desc = "split code block" },
+        { "<leader>ct", "<cmd>TSJJoin<cr>",   desc = "Join code block" },
     },
     opts = {
         use_default_keymaps = false,
