c6973cffb5c887dad3ba83ba41503669e396d5f8
Sawan Sunar <sawansunar24072002@gmail.com>
2024-05-03 16:24:12 +0530

    Added Grapple and arrow

---
+++ lua/plugins/arrow.lua
@@ -0,0 +1,9 @@
+return {
+  "otavioschwanck/arrow.nvim",
+  event = "VeryLazy",
+  opts = {
+    show_icons = true,
+    leader_key = "<leader>ml", -- Recommended to be a single key
+    buffer_leader_key = "<leader>mf", -- Per Buffer Mappings
+  }
+}
