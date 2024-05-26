8fa8e32cabc80d2fb3b08d470a056fd7be6202fa
Sawan Sunar <sawansunar24072002@gmail.com>
2024-05-26 22:10:16 +0530

    added note on keymaps for maps.

--- lua/plugins/marks.lua
+++ lua/plugins/marks.lua
@@ -1,40 +1,58 @@
 return {
-    'chentoast/marks.nvim',
+    "chentoast/marks.nvim",
     event = "VeryLazy",
     keys = {
         {
-            '<leader>mat',
+            "<leader>mat",
             ":MarksToggleSigns<cr>",
-            mode = { 'n' },
+            mode = { "n" },
             desc = "Marks toggle signs"
         },
         {
-            '<leader>malb',
+            "<leader>malb",
             ":MarksListBuf<cr>",
-            mode = { 'n' },
+            mode = { "n" },
             desc = "Marks open in localist in current buffers"
         },
         {
-            '<leader>malg',
+            "<leader>malg",
             ":MarksListGlobal<cr>",
-            mode = { 'n' },
+            mode = { "n" },
             desc = "Marks open in global localist in current buffers"
         },
         {
-            '<leader>maqb',
+            "<leader>maqb",
             ":MarksQFListBuf<cr>",
-            mode = { 'n' },
+            mode = { "n" },
             desc = "Marks open in quicklist in current buffers"
         },
         {
-            '<leader>maqg',
+            "<leader>maqg",
             ":MarksQFListGlobal<cr>",
-            mode = { 'n' },
+            mode = { "n" },
             desc = "Marks open global in quicklist in current buffers"
         },
     },
     config = function()
-        require 'marks'.setup {
+        -- mx              Set mark x
+        -- m,              Set the next available alphabetical (lowercase) mark
+        -- m;              Toggle the next available mark at the current line
+        -- dmx             Delete mark x
+        -- dm-             Delete all marks on the current line
+        -- dm<space>       Delete all marks in the current buffer
+        -- m]              Move to next mark
+        -- m[              Move to previous mark
+        -- m:              Preview mark. This will prompt you for a specific mark to
+        --                 preview; press <cr> to preview the next mark.
+        --
+        -- m[0-9]          Add a bookmark from bookmark group[0-9].
+        -- dm[0-9]         Delete all bookmarks from bookmark group[0-9].
+        -- m}              Move to the next bookmark having the same type as the bookmark under
+        --                 the cursor. Works across buffers.
+        -- m{              Move to the previous bookmark having the same type as the bookmark under
+        --                 the cursor. Works across buffers.
+        -- dm=             Delete the bookmark under the cursor.
+        require "marks".setup {
             default_mappings = true,
             signs = false,
             builtin_marks = { ".", "<", ">", "^" },
