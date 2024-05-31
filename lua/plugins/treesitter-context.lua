b65aa99b3175a3af92b7abdd37246178e66539e9
Sawan Sunar <sawansunar24072002@gmail.com>
2024-05-31 16:25:01 +0530

    jump to context keymap

--- lua/plugins/treesitter-context.lua
+++ lua/plugins/treesitter-context.lua
@@ -1,6 +1,15 @@
 return {
     "nvim-treesitter/nvim-treesitter-context",
     lazy = true,
+    keys = {
+        {
+            "[t",
+            function()
+                require("treesitter-context").go_to_context(vim.v.count1)
+            end,
+            desc = "jump to context"
+        }
+    },
     opts = {
         max_lines = 5,
     }
