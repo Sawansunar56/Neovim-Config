f62b4d29246421a754138ba43838572602087f97
Sawan Sunar <sawansunar24072002@gmail.com>
2024-06-26 23:05:46 +0530

    IDEA todo comments

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -31,6 +31,7 @@ return {
                 TODO = { icon = " ", color = "info" },
                 HACK = { icon = " ", color = "warning" },
                 WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+                IDEA = { icon = " ", },
                 PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
                 NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
                 TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
