85c9ad9fa8268ae98fbe6f69388db4a1d45d7140
Sawan Sunar <sawansunar24072002@gmail.com>
2024-12-22 12:47:14 +0530

    decrease tab size to 2 spaces

--- lua/sawan/set.lua
+++ lua/sawan/set.lua
@@ -11,9 +11,9 @@ opt.relativenumber = true
 
 opt.ignorecase = true
 opt.smartcase = true
-opt.tabstop = 4
-opt.softtabstop = 4
-opt.shiftwidth = 4
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
 opt.expandtab = true
 opt.smarttab = true
 
