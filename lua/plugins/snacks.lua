c93a943bfdb05a6e9e5e5da53f65b58362efe0d3
Sawan Sunar <sawansunar24072002@gmail.com>
2025-07-10 13:45:20 +0530

    todo comments picker mapped to a key

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,25 +1,27 @@
 return {
-    "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
-    keys = {
-        { "<leader>ks", function() Snacks.scratch() end,        desc = "Toggle Scratch Buffer" },
-        { "<leader>kw", function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
-        { "<leader>ka", function() Snacks.picker() end,         desc = "Select pickers" },
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  keys = {
+    { "<leader>ks", function() Snacks.scratch() end,              desc = "Toggle Scratch Buffer" },
+    { "<leader>kw", function() Snacks.scratch.select() end,       desc = "Snacks Scratch Buffer" },
+    { "<leader>ka", function() Snacks.picker() end,               desc = "Snacks pickers" },
+    { "<leader>kt", function() Snacks.picker.todo_comments() end, desc = "Snacks Select todo comments" },
+    { "<leader>kr", function() Snacks.picker.resume() end,        desc = "Snacks Resume" },
+  },
+  opts = {
+    scratch = {
     },
-    opts = {
-        scratch = {
-        },
-        picker = {
-        },
-        bigfile = { enabled = false },
-        dashboard = { enabled = false },
-        indent = { enabled = false },
-        input = { enabled = false },
-        notifier = { enabled = false },
-        quickfile = { enabled = false },
-        scroll = { enabled = false },
-        statuscolumn = { enabled = false },
-        words = { enabled = false },
-    }
+    picker = {
+    },
+    bigfile = { enabled = false },
+    dashboard = { enabled = false },
+    indent = { enabled = false },
+    input = { enabled = false },
+    notifier = { enabled = false },
+    quickfile = { enabled = false },
+    scroll = { enabled = false },
+    statuscolumn = { enabled = false },
+    words = { enabled = false },
+  }
 }
